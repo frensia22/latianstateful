533190299a73a7854bf7104a890c120dc4584e02
frensia22 <frensiaclaudia22@gmail.com>
2021-04-27 16:43:04 +0800

    latian

---
+++ flutter_application_1/lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int number = 1;
+  void tekanTombol() {
+    setState(() {
+      number = number + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Latihan Statefull")),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(number.toString()),
+              MaterialButton(
+                  color: Colors.blue,
+                  height: 50,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(5.0)),
+                  child: Text("Tambah"),
+                  onPressed: tekanTombol)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
